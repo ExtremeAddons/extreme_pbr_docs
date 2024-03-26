75bd6c2d3e063b8b4348cede4097692f383d0bdf
Extreme Addons <86814412+ExtremeAddons@users.noreply.github.com>
2024-03-26 10:41:33 +0100

    Initial Commit

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.1.122',
+    VERSION: '4.1.123',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@
 project = 'Extreme PBR'
 copyright = '2024, Andrea Donati'
 author = 'Andrea Donati'
-release = '4.1.122'
+release = '4.1.123'
 
 import os, sys
 
